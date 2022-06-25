c47d8b2778f76e2d0dd7654680293b4a4195d48d
Luc Ho <hotriluc@Lucs-MacBook-Pro.local>
2022-06-25 17:25:24 +0200

    music control layout

--- src/components/MusicPlayer/MusicPlayerNavigation.tsx
+++ src/components/MusicPlayer/MusicPlayerNavigation.tsx
@@ -3,6 +3,14 @@ import styles from './MusicPlayerNavigation.module.css';
 import { useSelector } from 'react-redux';
 import { IRootState } from '../../store/RootState';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faPlay,
+  // faPause,
+  faForward,
+  faBackward,
+} from '@fortawesome/free-solid-svg-icons';
+
 function MusicPlayerNavigation(): JSX.Element {
   const current = useSelector(
     (state: IRootState) => state.musicPlayer.currentSong
@@ -11,10 +19,16 @@ function MusicPlayerNavigation(): JSX.Element {
   return (
     <div className={styles['navigation-wrapper']}>
       <img className={styles['cover-img']} src={current.coverImage} alt="" />
-      <div>
-        <button>back</button>
-        <button>stop</button>
-        <button>next</button>
+      <div className={styles['music-player-controls']}>
+        <button className={styles['back-btn']}>
+          <FontAwesomeIcon icon={faBackward} />
+        </button>
+        <button className={styles['play-btn']}>
+          <FontAwesomeIcon icon={faPlay} />
+        </button>
+        <button className={styles['next-btn']}>
+          <FontAwesomeIcon icon={faForward} />
+        </button>
       </div>
     </div>
   );
