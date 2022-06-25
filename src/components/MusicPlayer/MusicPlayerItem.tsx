af09d7beb5f64a72dbac134903f984044bcf3caf
Luc Ho <hotriluc@Lucs-MacBook-Pro.local>
2022-06-25 17:39:01 +0200

    border color when selected song

--- src/components/MusicPlayer/MusicPlayerItem.tsx
+++ src/components/MusicPlayer/MusicPlayerItem.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import styles from '../MusicPlayer/MusicPlayerItem.module.css';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { musicPlayerActions } from '../../store/music-player';
+import { IRootState } from '../../store/RootState';
 
 interface MusicPlayerItemProps {
   id: string;
@@ -18,8 +19,17 @@ function MusicPlayerItem(props: MusicPlayerItemProps): JSX.Element {
     dispatch(musicPlayerActions.selectCurrentSong(props.id));
   };
 
+  const currentSong = useSelector(
+    (state: IRootState) => state.musicPlayer.currentSong
+  );
+
+  const activeSongClasses =
+    currentSong.id === props.id
+      ? `${styles['song']} ${styles['active-song']}`
+      : `${styles['song']}`;
+
   return (
-    <li className={styles['song']} onClick={onClickSongHandler}>
+    <li className={activeSongClasses} onClick={onClickSongHandler}>
       <div className={styles['img-holder']}>
         <img className={styles['cover-img']} src={props.coverImage} alt="" />
       </div>
